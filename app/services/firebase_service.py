20ec455ed806ea12ee61123f974d45430a33c007
kintu hamuzah <kintuhamuzah66@gmail.com>
2026-05-16 05:53:05 +0300

    Secure Firebase setup for Render

--- app/services/firebase_service.py
+++ app/services/firebase_service.py
@@ -1,19 +1,30 @@
+import os
+import json
 import firebase_admin
 
-from firebase_admin import credentials, db
+from firebase_admin import credentials
+from firebase_admin import db
+
 from app.config import FIREBASE_URL
+
 
 # Prevent duplicate Firebase initialization
 if not firebase_admin._apps:
 
-    cred = credentials.Certificate(
-        "learning-for-project-e4909-firebase-adminsdk-fbsvc-4f5dddb170.json"
+    firebase_credentials = json.loads(
+        os.getenv("FIREBASE_CREDENTIALS")
     )
 
-    firebase_admin.initialize_app(cred, {
-        "databaseURL":
-        FIREBASE_URL
-    })
+    cred = credentials.Certificate(
+        firebase_credentials
+    )
+
+    firebase_admin.initialize_app(
+        cred,
+        {
+            "databaseURL": FIREBASE_URL
+        }
+    )
 
 
 # Function to save phone events
